150f2700927ac7dc91d23cd4e23d7fb6593ce83d
IMadering <delferon@gmail.com>
2025-11-13 16:07:39 +0300

    Add files via upload

---
+++ web.bat
@@ -0,0 +1,7 @@
+::@echo off
+setlocal
+set path=%~d0%~p0
+
+"C:\Tools\ungoogled-chromium_129.0.6668.70-1.1_windows\chrome.exe" --allow-file-access-from-files --disable-web-security --user-data-dir="C:\Temp\chrome_dev" "%path%index.html"
+
+endlocal
